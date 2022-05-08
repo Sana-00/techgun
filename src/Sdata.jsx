8acd57649bd8705fabd3ed107ced6da3f663d96e
sana <sana@gmail.com>
2022-05-08 07:21:10 +0500

    react practise website

---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+import web from  "../src/images/web.png";
+import app  from  "../src/images/app.png";
+import android from  "../src/images/android.png";
+import marketing from  "../src/images/marketig.png";
+import  digital from  "../src/images/digital.png";
+import software  from  "../src/images/software.png";
+
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title:"web development",
+    },
+    {
+        imgsrc: app ,
+        title:"application development",
+    },
+    {
+        imgsrc: android ,
+        title:"android development",
+    },
+    {
+        imgsrc: digital,
+        title:"digital marketing",
+    },
+    {
+        imgsrc: marketing ,
+        title:"marketing",
+    },
+    {
+        imgsrc: software,
+        title:"software development",
+    },
+];
+
+
+export default Sdata;
